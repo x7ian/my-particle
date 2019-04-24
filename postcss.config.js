70507b95010fb5600d72ab4f69ffd0c5ec85344a
Christopher Bloom <bloomcb@gmail.com>
2019-04-24 16:05:28 -0700

    PostCSS nested

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,13 @@
 const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
 const tailwindcss = require('tailwindcss');
+const nested = require('postcss-nested');
 
 module.exports = ({ options, env }) => {
   return {
     plugins: [
+      // Nest like Sass
+      nested(),
       // tailwindConfig is set per *design system* webpack.config.js.
       options.tailwindConfig && tailwindcss(options.tailwindConfig),
       // Use .browserslistrc to determine CSS mutations
